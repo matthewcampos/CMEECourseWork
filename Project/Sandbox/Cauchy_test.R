4800cf4159cba5d0453d5070112e073742c15b5d
Matthew Campos <matthew.campos19@imperial.ac.uk>
2020-08-20 19:01:44 -0700

    remove of wrong data

--- Project/Sandbox/Cauchy_test.R
+++ Project/Sandbox/Cauchy_test.R
@@ -1,6 +1,10 @@
 x <- c(1:120)
 val <- dcauchy(x,50,8)
-val2 <- dcauchy(x,35,10)
+val2 <- dcauchy(x,65,10)
+val3 <- dcauchy(x,80,10)
 
-plot(x,val,type = 'l',col='blue',ylim = c(0,0.1))
+plot(x,val,type = 'l',col='black',xlab='Trait Values',ylab='Fitness Probability',main='Cauchy Distribution of Environmental Distance',ylim=c(0,0.045))
 lines(x,val2, type ='l',col='red')
+lines(x,val3,type='l',col='blue')
+legend(80, 0.0465, legend=c("Trait Value 50", "Trait Value 65",'Trait Value 80'),
+       col=c("black", "blue",'red'), lty=1,cex=0.8)
